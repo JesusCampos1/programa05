12706d9a014113963843b82b88c90fd1c6c06f92
jsusc <jsusc@DESKTOP-SKGNO7S>
2019-01-31 19:48:53 -0600

    programa05

---
+++ programa05/src/programa05/programa05.java
@@ -0,0 +1,21 @@
+package programa05;
+
+import java.util.Scanner;
+
+public class programa05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double calificacion;
+		Scanner leer = new Scanner(System.in);
+		System.out.print("Ingrese su calificacion: ");
+		calificacion = leer.nextDouble();
+		if (calificacion>=80) {
+			System.out.println("Aprobado");
+		} else { 
+				System.out.println("Reprobado");
+		
+
+    }
+	}
+	}
